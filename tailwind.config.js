871f03bc0cdf596d2e0b4c3949a940aaf552d0f2
Vikash Kumar <mailkumarvikash@gmail.com>
2024-09-15 19:55:42 -0700

    setup postcss and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './*.html',
+    './*.md',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
