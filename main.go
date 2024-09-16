e18dbc4ee14473b3d88cacbea54be5ba944da9b4
Vikash Kumar <mailkumarvikash@gmail.com>
2024-09-15 19:57:17 -0700

    setup go app for app engine

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
-
-	"vikash.me/apollo/app/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.HellowordHandler)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+	http.ListenAndServe(":8080", nil)
 }
